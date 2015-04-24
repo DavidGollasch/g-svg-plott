0df31c956ded1450f343367c0f9dc2f4fa658e9e
David Gollasch <david@gollasch-it.de>
2015-04-24 18:43:31 +0200

    PersistanceHelper implemented that allows manageing file storage and loading (application state and styles)

---
+++ gSVGPlott/src/de/tudresden/inf/gsvgplott/ui/util/PersistanceHelper.java
@@ -0,0 +1,126 @@
+/**
+ * 
+ */
+package de.tudresden.inf.gsvgplott.ui.util;
+
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.List;
+
+import de.tudresden.inf.gsvgplott.data.Diagram;
+import de.tudresden.inf.gsvgplott.data.Function;
+import de.tudresden.inf.gsvgplott.data.MarkedPointsList;
+
+/**
+ * @author David Gollasch
+ *
+ */
+public class PersistanceHelper {
+	/**
+	 * Method to store a diagram state as xml file
+	 * @param diagram
+	 * @param filename
+	 */
+	public static void storeDiagramToFile(Diagram diagram, String filename) throws Exception {
+		XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(filename)));
+		encoder.writeObject(diagram);
+		encoder.close();
+	}
+	
+	/**
+	 * Method to store style as an xml file (store diagram without data)
+	 * @param style
+	 * @param filename
+	 * @return
+	 */
+	public static boolean storeStyleToFile(Diagram style, String filename) throws Exception {
+		//consider removing data here later on
+		
+		XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(filename)));
+		encoder.writeObject(style);
+		encoder.close();
+		return true;
+	}
+	
+	/**
+	 * Loads diagram by file
+	 * @param filename
+	 * @return
+	 * @throws Exception
+	 */
+	public static Diagram loadDiagramFromFile(String filename) throws Exception {
+		XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream(filename)));
+	    Diagram o = (Diagram)decoder.readObject();
+	    decoder.close();
+	    return o;
+	}
+	
+	public static Diagram loadStyleFromFile(String filename) throws Exception {
+		XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream(filename)));
+	    Diagram o = (Diagram)decoder.readObject();
+	    decoder.close();
+	    return o;
+	}
+	
+	/**
+	 * Apply styling information onto an data diagram object
+	 * @param data Diagram object to apply styling on
+	 * @param style Style information source
+	 * @return Completely styled diagram object
+	 */
+	public static Diagram applyStyleToDiagram(Diagram data, Diagram style) {
+		data.setBackgroundScreenStyle(style.getBackgroundScreenStyle());
+		data.setBackgroundPrintStyle(style.getBackgroundPrintStyle());
+		
+		data.setGridScreenStyle(style.getGridScreenStyle());
+		data.setGridPrintStyle(style.getGridPrintStyle());
+		
+		data.setTextScreenStyle(style.getTextScreenStyle());
+		data.setTextPrintStyle(style.getTextPrintStyle());
+		
+		data.setOverrideStyle(style.getOverrideStyle());
+		
+		data.getXaxis().setAxisScreenStyle(style.getXaxis().getAxisScreenStyle());
+		data.getXaxis().setAxisPrintStyle(style.getXaxis().getAxisPrintStyle());
+		data.getXaxis().setHelplineScreenStyle(style.getXaxis().getHelplineScreenStyle());
+		data.getXaxis().setHelplinePrintStyle(style.getXaxis().getHelplinePrintStyle());
+		
+		data.getYaxis().setAxisScreenStyle(style.getYaxis().getAxisScreenStyle());
+		data.getYaxis().setAxisPrintStyle(style.getYaxis().getAxisPrintStyle());
+		data.getYaxis().setHelplineScreenStyle(style.getYaxis().getHelplineScreenStyle());
+		data.getYaxis().setHelplinePrintStyle(style.getYaxis().getHelplinePrintStyle());
+		
+		if(data.getIntegral() != null && style.getIntegral() != null) {
+			data.getIntegral().setAreaScreenStyle(style.getIntegral().getAreaScreenStyle());
+			data.getIntegral().setAreaPrintStyle(style.getIntegral().getAreaPrintStyle());
+		}
+		
+		List<Function> dataFunctions = data.getFunctions();
+		List<Function> styleFunctions = style.getFunctions();
+		for(Function f : dataFunctions) {
+			int index = dataFunctions.indexOf(f);
+			if(index < styleFunctions.size()) {
+				Function styleF = styleFunctions.get(index);
+				f.setFunctionScreenStyle(styleF.getFunctionScreenStyle());
+				f.setFunctionPrintStyle(styleF.getFunctionPrintStyle());
+			}
+		}
+		
+		List<MarkedPointsList> dataLists = data.getMarkedPointLists();
+		List<MarkedPointsList> styleLists = style.getMarkedPointLists();
+		for(MarkedPointsList l : dataLists) {
+			int index = dataLists.indexOf(l);
+			if(index < styleLists.size()) {
+				MarkedPointsList styleL = styleLists.get(index);
+				l.setPointScreenStyle(styleL.getPointScreenStyle());
+				l.setPointPrintStyle(styleL.getPointPrintStyle());
+			}
+		}
+		
+		return data;
+	}
+}
